9dfa557234c2d366859baeebf8a618d4bad1418c
Ahmad <gracenoteii5@gmail.com>
2021-02-27 22:28:22 +0500

    added winner

--- Pong Game/Pong_Game.py
+++ Pong Game/Pong_Game.py
@@ -1,4 +1,9 @@
 import turtle
+import time
+import sys
+
+
+seconds=int(input("For how many Seconds you want to play  "))
 
 
 wn=turtle.Screen()
@@ -54,6 +59,15 @@ pen.hideturtle()
 pen.goto(0,260)
 pen.write("Player A: 0   Player B: 0",align="center",font=("Courier",18,"normal"))
 
+#timerpen
+timer=turtle.Turtle()
+timer.speed(0)
+timer.color("white")
+timer.pendown
+timer.hideturtle()
+timer.goto(0,-260)
+timer.write("Time: 0",align="center",font=("Courier",18,"normal"))
+
 
 #Function
 def paddle_a_up():
@@ -85,6 +99,7 @@ def paddle_b_down():
         y-=20
     paddle_b.sety(y)
 
+
 #keyboard binding
 
 wn.listen()
@@ -93,50 +108,64 @@ wn.onkeypress(paddle_a_down,"s")
 wn.onkeypress(paddle_b_up,"Up")
 wn.onkeypress(paddle_b_down,"Down")
 
-
+starttime=time.time()
 
 #Main game Loop
 while True:
-    wn.update()
+        wn.update()
+        elapasedtime=time.time()-starttime
+        elapasedtime=format(elapasedtime,",.0f")
+        timer.clear()
+        timer.write("Time: {} seconds".format(elapasedtime),align="center",font=("Courier",18,"normal"))
+        endtime=int(elapasedtime)
+        if(seconds==endtime):
+             timer.clear()
+             pen.clear()
+             timer.goto(0,0)
+             if score_a>score_b:
+                    timer.write("Player A Wins\nExiting...",align="center",font=("Courier",20,"normal"))
+             elif score_b>score_a:
+                    timer.write("Player B Wins\nExiting...",align="center",font=("Courier",20,"normal"))
+             else:
+                    timer.write("Draw\nExiting...",align="center",font=("Courier",20,"normal"))
+             time.sleep(10)
+             break
+            
+        
+        #move the ball
+        ball.setx(ball.xcor()+ball.dx)
+        ball.sety(ball.ycor()+ball.dy)
 
-    #move the ball
-    ball.setx(ball.xcor()+ball.dx)
-    ball.sety(ball.ycor()+ball.dy)
 
+        #bordercheck
+        if ball.ycor()>290:
+            ball.sety(290)
+            ball.dy*=-1
 
-    #bordercheck
-    if ball.ycor()>290:
-        ball.sety(290)
-        ball.dy*=-1
-
-    if ball.ycor()<-290:
-        ball.sety(-290)
-        ball.dy*=-1
+        if ball.ycor()<-290:
+            ball.sety(-290)
+            ball.dy*=-1
     
-    if ball.xcor()>390:
-        ball.goto(0,0)
-        ball.dx*=-1
+        if ball.xcor()>390:
+            ball.goto(0,0)
+            ball.dx*=-1
 
-    if ball.xcor()<-390:
-        ball.goto(0,0)
-        ball.dx*=-1
-
-
-    if(ball.xcor()>340 and ball.xcor()<350)and (ball.ycor()<paddle_b.ycor()+40 and ball.ycor()>paddle_b.ycor()-40):
-        ball.setx(340)
-        ball.dx*=-1
-        score_b+=1
-        pen.clear()
-        pen.write("Player A: {}   Player B: {}".format(score_a,score_b),align="center",font=("Courier",18,"normal"))
+        if ball.xcor()<-390:
+            ball.goto(0,0)
+            ball.dx*=-1
 
 
-    if(ball.xcor()<-340 and ball.xcor()>-350)and (ball.ycor()<paddle_a.ycor()+40 and ball.ycor()>paddle_a.ycor()-40):
-        ball.setx(-340)
-        ball.dx*=-1
-        score_a+=1
-        pen.clear()
-        pen.write("Player A: {}   Player B: {}".format(score_a,score_b),align="center",font=("Courier",18,"normal"))
+        if(ball.xcor()>340 and ball.xcor()<350)and (ball.ycor()<paddle_b.ycor()+40 and ball.ycor()>paddle_b.ycor()-40):
+            ball.setx(340)
+            ball.dx*=-1
+            score_b+=1
+            pen.clear()
+            pen.write("Player A: {}   Player B: {}".format(score_a,score_b),align="center",font=("Courier",18,"normal"))
 
 
-
-
+        if(ball.xcor()<-340 and ball.xcor()>-350)and (ball.ycor()<paddle_a.ycor()+40 and ball.ycor()>paddle_a.ycor()-40):
+            ball.setx(-340)
+            ball.dx*=-1
+            score_a+=1
+            pen.clear()
+            pen.write("Player A: {}   Player B: {}".format(score_a,score_b),align="center",font=("Courier",18,"normal"))
